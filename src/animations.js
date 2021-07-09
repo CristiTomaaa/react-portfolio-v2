498d5cb596f298882b0a5bc8c41c2b876c450398
CristiTomaaa <cristi.toma1990@gmail.com>
2021-07-09 15:37:39 +0100

    added page transition animations, home page and portfolio

--- src/animations.js
+++ src/animations.js
@@ -8,13 +8,51 @@ export const pageAnimations = {
     y: 0,
     transition: {
       duration: 0.7,
+      when: "beforeChildren",
+      staggerChildren: 0.25,
     },
   },
   exit: {
     opacity: 0,
     y: 300,
     transition: {
-      duration: 0.7,
+      duration: 0.5,
     },
+  },
+};
+
+export const titleAnimation = {
+  hidden: { y: 200 },
+  show: {
+    y: 0,
+    transition: { duration: 0.75, ease: "easeOut" },
+  },
+};
+
+export const fade = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: { ease: "easeOut", duration: 0.75 },
+  },
+};
+
+export const photoAnimation = {
+  hidden: { scale: 1.5, opacity: 0 },
+  show: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      ease: "easeOut",
+      duration: 0.75,
+    },
+  },
+};
+
+export const lineAnimation = {
+  hidden: { width: "0%" },
+  show: {
+    width: "100%",
+    transition: { duration: 1 },
   },
 };
